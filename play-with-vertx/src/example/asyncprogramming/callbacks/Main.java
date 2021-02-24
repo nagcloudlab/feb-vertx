9ff31f0600dbd443dcd15d68e2f87569f14605a5
Nagabhushanam <nagabhushanamn@gmail.com>
2021-02-24 10:49:01 +0530

    async programming with callbacks

---
+++ play-with-vertx/src/example/asyncprogramming/callbacks/Main.java
@@ -0,0 +1,37 @@
+package example.asyncprogramming.callbacks;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Vertx vertx=Vertx.vertx();
+		
+		
+		vertx.deployVerticle("example.asyncprogramming.sensor.HeatSensor",
+				new DeploymentOptions()
+				.setConfig(new JsonObject()
+						.put("http.port", 3001)));
+		
+		vertx.deployVerticle("example.asyncprogramming.sensor.HeatSensor",
+				new DeploymentOptions()
+				.setConfig(new JsonObject()
+						.put("http.port", 3002)));
+		
+		vertx.deployVerticle("example.asyncprogramming.sensor.HeatSensor",
+				new DeploymentOptions()
+				.setConfig(new JsonObject()
+						.put("http.port", 3003)));
+		
+		vertx.deployVerticle("example.asyncprogramming.snapshot.SnapshotService",
+				new DeploymentOptions());
+		
+		vertx.deployVerticle("example.asyncprogramming.callbacks.EdgeService",
+				new DeploymentOptions());
+		
+	}
+
+}
