06637abe4f7d285159cff1dcfd80964d740fb084
Nagabhushanam <nagabhushanamn@gmail.com>
2021-02-25 10:55:58 +0530

    vertx-wiki service

---
+++ vertx-wiki-step-1/src/main/java/io/vertx/guides/wiki/Main.java
@@ -0,0 +1,16 @@
+package io.vertx.guides.wiki;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Vertx vertx=Vertx.vertx();
+		vertx.deployVerticle("io.vertx.guides.wiki.MainVerticle",new DeploymentOptions().setInstances(2));
+		
+		
+	}
+
+}
